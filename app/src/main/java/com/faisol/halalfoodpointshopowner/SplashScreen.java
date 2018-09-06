31082d54cf68e778a77c58f77847092e189d5cdc
faisol26150 <faisolhayee135@gmail.com>
2018-09-07 00:55:18 +0700

    Add SplashScreen

---
+++ app/src/main/java/com/faisol/halalfoodpointshopowner/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.faisol.halalfoodpointshopowner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(3000)
+                .withBackgroundResource(R.drawable.back2)
+                .withLogo(R.drawable.logoshop1);
+
+        View view = config.create();
+        setContentView(view);
+
+    }
+}
